15de30378741ba5987c36b8bd4c4a72e9bb25de3
Tflyer <teemingflyer@gmail.com>
2018-01-28 18:42:25 -0500

    Xmr V1

---
+++ start_etn.bat
@@ -0,0 +1,1 @@
+NsCpuCNMiner64.exe -o stratum+ssl://etn-eu1.nanopool.org:13433 -u YOUR_ETN_ADDRESS.YOUR_PAYMENT_ID.YOUR_WORKER/YOUR_EMAIL -p x -allpools 1
---
+++ start_xmr.bat
@@ -0,0 +1,1 @@
+NsCpuCNMiner64.exe -o stratum+ssl://xmr-eu1.nanopool.org:14433 -u YOUR_XMR_ADDRESS.YOUR_PAYMENT_ID.YOUR_WORKER/YOUR_EMAIL -p x
