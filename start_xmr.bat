676209fbe247efa59f342c995cc9bb3025611dfc
teemingflyer <teemingflyer@gmail.com>
2018-01-28 18:47:19 -0500

    Update start_xmr.bat

--- start_xmr.bat
+++ start_xmr.bat
@@ -1,1 +1,6 @@
-NsCpuCNMiner64.exe -o stratum+ssl://xmr-eu1.nanopool.org:14433 -u YOUR_XMR_ADDRESS.YOUR_PAYMENT_ID.YOUR_WORKER/YOUR_EMAIL -p x
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+NsCpuCNMiner64.exe -xpool ssl://xmr-us-east1.nanopool.org:14433 -xwal  43VXR67CfX5NcbXna9EZjwFnBhGrnqxc4Usw2CGxrGH8YaiUPfTsBacLgVcCmUrWGqdCVMsLDhbj1CDjNs5KgKcg98C8XH5s.Monero-M01/teemingflyer@gmail.com -xpsw z
